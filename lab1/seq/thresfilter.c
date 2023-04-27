f1ed5dd10753b03beb2305442d64a124eee20fb0
mfroeh <mfroeh0@pm.me>
2023-04-27 10:25:53 +0200

    Prepare for lab2

---
+++ lab1/seq/thresfilter.c
@@ -0,0 +1,28 @@
+#include "thresfilter.h"
+#define uint unsigned int 
+
+void thresfilter(const int xsize, const int ysize, pixel* src)
+{
+	uint sum, i, psum, nump;
+	nump = xsize * ysize;
+	
+	for (i = 0, sum = 0; i < nump; i++)
+	{
+		sum += (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+	}
+	
+	sum /= nump;
+	
+	for (i = 0; i < nump; i++)
+	{
+		psum = (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+		if (sum > psum)
+		{
+			src[i].r = src[i].g = src[i].b = 0;
+		}
+		else
+		{
+			src[i].r = src[i].g = src[i].b = 255;
+		}
+	}
+}
