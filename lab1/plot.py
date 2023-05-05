2014a2e77062d370b75040e73c8fe166dba3c542
mfroeh <mfroeh0@pm.me>
2023-05-05 09:08:09 +0200

    Add plot for lab2

--- lab1/plot.py
+++ lab1/plot.py
@@ -1,29 +1,36 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Data
 x = [1, 2, 4, 8, 16, 32, 64]
-blur = [16.3786, 8.55247, 4.32111, 2.21067, 1.26054, 0.982082, 1.17529]
-threshold = [0.088511, 0.062794, 0.031244, 0.020858, 0.016567, 0.013478, 0.008912]
+blur_pthreads = [16.3786, 8.55247, 4.32111, 2.21067, 1.26054, 0.982082, 1.17529]
+threshold_mpi = [0.088511, 0.062794, 0.031244, 0.020858, 0.016567, 0.013478, 0.008912]
+threshold_pthreads = [0.109217, 0.0535817, 0.0371549, 0.0252247, 0.0157444, 0.0183072, 0.0130858]
+blur_mpi = [14.018321, 7.058290, 3.750033, 1.937216, 1.107606, 0.754410, 0.656233]
+lapl_omp = [0.181218, 0.104422, 0.064208, 0.043191, 0.034682, 0.051359]
 
-# Create figure and two subplots
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+# create subplots
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
 
-# Plot 1: Blur filter
-ax1.plot(x, blur)
-ax1.set_title('Blur Filter')
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
+# plot for blur
+ax1.plot(x, blur_pthreads, 'ro-', label='Pthreads')
+ax1.plot(x, blur_mpi, 'go-', label='MPI')
+ax1.set_title('Blur Execution Time')
+ax1.set_xlabel('Threads/Processes')
+ax1.set_ylabel('Execution Time (s)')
+ax1.legend()
 
-# Plot 2: Threshold filter
-ax2.plot(x, threshold)
-ax2.set_title('Threshold Filter')
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
+# plot for threshold
+ax2.plot(x, threshold_pthreads, 'ro-', label='Pthreads')
+ax2.plot(x, threshold_mpi, 'go-', label='MPI')
+ax2.set_title('Threshold Execution Time')
+ax2.set_xlabel('Threads/Processes')
+ax2.set_ylabel('Execution Time (s)')
+ax2.legend()
 
-# Adjust spacing between subplots
-plt.subplots_adjust(hspace=0.5)
+# plot for lapl_omp
+ax3.plot(x, lapl_omp, 'bo-', label='OpenMP')
+ax3.set_title('Laplacian Execution Time')
+ax3.set_xlabel('Threads')
+ax3.set_ylabel('Execution Time (s)')
+ax3.legend()
 
-# Show plot
 plt.show()
-
